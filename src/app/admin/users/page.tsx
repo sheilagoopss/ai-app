dd39346f6058748210ad827fccf7d56082947d1e
natnael <nhattymack204@gmail.com>
2025-01-01 18:33:09 +0300

    feat(admin-users): add search functionality to user table and improve layout

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,15 +1,21 @@
 "use client";
 import { useGetUsers } from "@/hooks/useUsers";
 import { IUser } from "@/types/account";
-import { Table } from "antd";
+import { Input, Table } from "antd";
 import { useEffect, useState } from "react";
+import { Content } from "antd/es/layout/layout";
+import { caseInsensitiveSearch } from "@/utils/caseInsensitveMatch";
 
 export default function AdminUsersPage() {
   const { getUsers, isFetchingUsers } = useGetUsers();
   const [users, setUsers] = useState<IUser[]>([]);
+  const [filteredUsers, setFilteredUsers] = useState<IUser[]>([]);
 
   useEffect(() => {
-    getUsers().then((users) => setUsers(users));
+    getUsers().then((users) => {
+      setUsers(users);
+      setFilteredUsers(users);
+    });
   }, [getUsers]);
 
   const columns = [
@@ -25,5 +31,30 @@ export default function AdminUsersPage() {
     },
   ];
 
-  return <Table dataSource={users} columns={columns} loading={isFetchingUsers} />;
+  const handleSearch = (searchTerm?: string) => {
+    const filterColumns: (keyof IUser)[] = ["email", "name"];
+    const filtered = users?.filter((val) =>
+      filterColumns.some((v) =>
+        caseInsensitiveSearch(val[v] || "", searchTerm),
+      ),
+    );
+    setFilteredUsers(filtered);
+  };
+
+  return (
+    <Content className="flex flex-col gap-4">
+      <div className="flex justify-between items-center">
+        <Input
+          placeholder="Search"
+          onChange={(e) => handleSearch(e.target.value)}
+          style={{ width: "50ch" }}
+        />
+      </div>
+      <Table
+        dataSource={filteredUsers}
+        columns={columns}
+        loading={isFetchingUsers}
+      />
+    </Content>
+  );
 }
